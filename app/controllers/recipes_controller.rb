58e1188cb179077791cf12de6c4eba827967a8f9
brian <riceb53@gmail.com>
2025-01-16 16:58:09 -0800

    dynamic recipes create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,12 +13,12 @@ class RecipesController < ApplicationController
   def create
     # how do i make a new recipe in a controller?
     @recipe = Recipe.new(
-      title: "chicken salad", 
-      ingredients: "chicken + lettuce", 
-      directions: "mix all ingredients together", 
-      prep_time: 20, 
-      chef: "brian",
-      image_url: "https://www.cookingclassy.com/wp-content/uploads/2024/05/grilled-chicken-salad-02.jpg"
+      title: params[:title], 
+      ingredients: params[:ingredients], 
+      directions: params[:directions], 
+      prep_time: params[:prep_time], 
+      chef: params[:chef],
+      image_url: params[:image_url]
     )
     @recipe.save
     render :show
