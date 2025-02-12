94d1f29c5b4653f40c8f4b154b697d80f51e5e08
brian <riceb53@gmail.com>
2025-02-11 17:22:50 -0800

    add signup

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ end
 
 
 gem "rack-cors"
+# Use ActiveModel has_secure_password
+gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_23_002610) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_12_004847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_23_002610) do
     t.string "directions"
     t.string "image_url"
     t.integer "prep_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
