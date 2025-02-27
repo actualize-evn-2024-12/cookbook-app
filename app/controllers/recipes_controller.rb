a616d4de9371f7520e5f98051fb8152fc2803ec5
brian <riceb53@gmail.com>
2025-02-26 18:38:49 -0800

    allow backend search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index   
-    @recipes = Recipe.all
+    # @recipes = Recipe.all
+    @recipes = Recipe.where("title LIKE ?", "%#{params[:search]}%")
     render :index
   end
 
